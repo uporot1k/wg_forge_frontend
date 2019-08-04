80246ad3f6afd288b520ada496abc8ce27d0d76c
Egor <uporot1k@yandex.ru>
2019-08-04 23:25:06 +0300

    add table params &&  set adapter

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import fetchData from './api/fetchData';
 import Table from './components/Table'
+import tableSchema from './helpers/tableSchema';
+import Adapter from './helpers/adapter';
 
 export default (async function () {
-    // YOUR CODE GOES HERE
-    // next line is for example only
-    let test = await fetchData('orders');
-    let table = new Table();
+    const adapter = new Adapter();
+
+    const tableHeader = tableSchema;
+    const tableData = await fetchData('orders');
+
+    let table = new Table({
+      el: '#app',
+      header: tableHeader,
+      data: tableData,
+      adapter: adapter.convertDataForTable,
+    });
     
-    document.getElementById("app").innerHTML = "<h1>Hello WG Forge</h1>";
 }());
